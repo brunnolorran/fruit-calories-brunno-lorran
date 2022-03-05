7c4915a19c26e573c9fd98ccb8800c7b2b6cabe4
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-05 20:00:13 -0300

    created header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+//Components
+import Header from "./Components/Header"
+
 import GlobalStyle from "./Styles/Global";
 
 function App() {
   return (
     <> 
+    <Header/>
     <div id="main-container">
       <h1>teste</h1>
     <GlobalStyle/>
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+    <>
+    <div class="wrapper">
+  <ul class="menu">
+    <li>Alpha</li>
+    <li>Beta</li>
+    <li>Gamma</li>
+  </ul>
+</div>
+    </>)
+}
+
+export default Header
