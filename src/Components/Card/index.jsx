dfcd61a7a6190ac0b3c6e1d4f1122ca86fa22979
brunnolorran <brunnolorran.silva@gmail..com>
2022-03-06 01:35:03 -0300

    ajusts map

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -10,13 +10,13 @@ const Card = ({
 }) => {
   return (
     <>
-      <h2>Nome: {name} Abacaxi</h2>
-      <p>Calorias: {calories} 48</p>
-      <p>Proteina: {protein} 0,54 g</p>
-      <p>Carboidratos: {carbohydrates} 12,63 g</p>
-      <p>Fibra: {fiber} 1,4 g</p>
-      <p>Gordura: {blubber} 0,12 g</p>
-      <p>portion: {portion} 1,2 fatias</p>
+      <h2>{name}</h2>
+      <p>Calorias: {calories}</p>
+      <p>Proteina: {protein}</p>
+      <p>Carboidratos: {carbohydrates}</p>
+      <p>Fibra: {fiber}</p>
+      <p>Gordura: {blubber}</p>
+      <p>portion: {portion}</p>
     </>
   );
 };
